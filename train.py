59faaa67ecd911c4cd78d9ad3f744f00237cb9d4
Sauching <67174563+LeeSauching@users.noreply.github.com>
2025-12-16 15:36:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from src.utils import seed_everything,, init_logger
 from src.data import load_and_expand, train_val_split, build_dataset
 #from src.model import load_model, create_trainer
 
-def train(fold):
+def train():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, required=True, help="Path to yaml config")
     args = parser.parse_args()
@@ -14,9 +14,14 @@ def train(fold):
       cfg = yaml.safe_load(f)
 
     seed_everything(cfg['seed'])
-    logger = init_looger(f"{cfg['run_name'].log}")
+    logger = init_logger(f"{cfg['run_name'].log}")
 
     logger.info(f"Loading model: {cfg['model']['path']}")
 
     df, test_df = load_and_expand(cfg['data']['comp_dir'])
+    train_df, val_df = train_val_split(df, cfg['data']['val_ratio'], cfg['seed'])
+
+	model, tokenizer = load_model_and_tokenizer(cfg['base_model_path'], cfg['max_seq_len'])
 	
+	train_ds = build_hf_dataset(train_df, tokenizer, cfg['prompt_variants'], use_log = True)
+
